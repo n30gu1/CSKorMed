e94b4089a68a33081927e02c56471d31ffefb167
n30gu1 <n@n30gu1.com>
2023-07-01 22:59:58 +0900

    Initial Commit

---
+++ CSKorMed/CSKorMedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSKorMedApp.swift
+//  CSKorMed
+//
+//  Created by Sung Park on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CSKorMedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSKorMed/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  CSKorMed
+//
+//  Created by Sung Park on 7/1/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
